1d2a820c33ce33a4769ec85f4cb6a03d08949d23
McEchean <983921194w@sina.com>
2018-04-25 18:21:56 +0800

    第一个接口不行

--- neteasy.py
+++ neteasy.py
@@ -10,8 +10,11 @@ import re
 import time
 import json
 import execjs
+import logging
 
 from http import cookiejar
+
+logging.basicConfig(level=logging.INFO)
 
 Headers = {
     'Accept': '*/*',
@@ -26,14 +29,15 @@ Post_param = {
     'domains': '163.com',
     'l': 0,
     'pd': 'mail163',
-    'pwdKeyUp': '1',
+    'pwdKeyUp': 1,
     'topURL': "https://mail.163.com/",
 }
+
 
 class Login_Neteasy_Mail():
     def __init__(self):
         self.session = requests.session()
-        self.session.headers =Headers.copy()
+        self.session.headers = Headers.copy()
         self.session.cookies = cookiejar.LWPCookieJar('cookie.txt')
         self._pkid = self._get_pkid()
         self._token = ''
@@ -47,15 +51,24 @@ class Login_Neteasy_Mail():
             'pkid': self._pkid,
             'tk': self._token,
             'un': username,
-            't': int(time.time()*1000),
+            't': int(time.time() * 1000),
             'pw': self._pw,
         })
-
+        login_url = 'https://dl.reg.163.com/l'
+        self.session.headers.update({
+            'Origin': 'https://dl.reg.163.com',
+            'Content-Type': 'application/json',
+        })
+        s = json.dumps(Post_param)
+        login_resp = self.session.post(url=login_url, data=s, headers=self.session.headers)
+        # self.session.cookies.save(ignore_discard=True, ignore_expires=True)
+        return login_resp.text
 
     def _get_pkid(self):
         pkid_url = 'https://mail.163.com/'
         get_pkid = re.match(r".*?(pkid:'(.*?)').*", self.session.get(url=pkid_url,
                                                                      headers=self.session.headers).text, re.S).group(2)
+        logging.info('get_pkid: %s' % get_pkid)
         return get_pkid
 
     def _get_token(self, username):
@@ -64,21 +77,53 @@ class Login_Neteasy_Mail():
                         'pkid={1}&' \
                         'pd=mail163&' \
                         'topURL=https%3A%2F%2Fmail.163.com%2F&' \
-                        'nocache={2}'.format(username, self._pkid, int(time.time()*1000))
-        return json.loads(self.session.get(url=get_token_url, headers=self.session.headers).text)['tk']
+                        'nocache={2}'.format(username, self._pkid, int(time.time() * 1000))
+        get_token_resp = self.session.get(url=get_token_url, headers=self.session.headers)
+        logging.info('get_token_resp: %s' % get_token_resp.text)
+        return json.loads(get_token_resp.text)['tk']
 
     def _get_init(self):
+        # self.session.headers.update({
+        #     'Host': 'mail.163.com',
+        #     'Referer': 'http://mail.163.com/logout.htm',
+        # })
+        # mail163_url = 'https://mail.163.com/'
+        # mail163_resp = self.session.get(url=mail163_url, headers=self.session.headers)
+        # logging.info('mail163_resp: %s' % mail163_resp.status_code)
+        self.session.headers.update({
+            'Host': 'dl.reg.163.com',
+            'Referer': 'https://mail.163.com/',
+        })
+        conf_init_url = 'https://dl.reg.163.com/getConf?callback=URSJSONP{0}&pkid=CvViHzl&pd=mail163&mode=1' \
+            .format(int(time.time() * 1000))
+        conf_init_resp = self.session.get(url=conf_init_url, headers=self.session.headers)
+        logging.info('conf_init_resp: %s' % conf_init_resp.text)
+        self.session.headers.update({
+            'Host': 'ir3.mail.163.com',
+        })
+        get_do_url = 'https://ir3.mail.163.com/get.do?prod=wmail_lbp&ver=1&uid=wy983821194@163.com&' \
+                     'domain=163.com&mobUser=0&callback=sing_{0}'.format(int(time.time() * 10000000))
+        get_do_resp = self.session.get(url=get_do_url, headers=self.session.headers)
+        logging.info('get_do_resp: %s' % get_do_resp.text)
+        self.session.headers.update({
+            'Host': 'ir.mail.163.com',
+        })
+        get_do2_url = 'https://ir.mail.163.com/get.do?uid=nt@163.com&domain=163.com&ver=4&ph=-1&' \
+                      'callback=sing_{0}'.format(int(time.time() * 10000000))
+        get_do2_resp = self.session.get(url=get_do2_url, headers=self.session.headers)
+        logging.info('get_do2_resp: %s' % get_do2_resp.text)
         self.session.headers.update({
             'Host': 'dl.reg.163.com',
             'Referer': 'https://dl.reg.163.com/webzj/m163_2/pub/index_dl.html?wdaId=&pkid=CvViHzl&product=mail163',
         })
-        get_init_utl = 'https://dl.reg.163.com/ini?' \
+        get_init_url = 'https://dl.reg.163.com/ini?' \
                        'pd=mail163&' \
                        'pkid={0}&' \
                        'pkht=mail.163.com&' \
                        'topURL=https%3A%2F%2Fmail.163.com%2F&' \
-                       'nocache={1}'.format(self._pkid, int(time.time()*1000))
-        self.session.get(url=get_init_utl,headers=self.session.headers)
+                       'nocache={1}'.format(self._pkid, int(time.time() * 1000))
+        _init_resp = self.session.get(url=get_init_url, headers=self.session.headers)
+        logging.info('_init_resp: %s' % _init_resp.text)
 
     def _get_js(self):
         f = open("encrypt.js", 'r', encoding='ISO-8859-1')
@@ -95,8 +140,5 @@ class Login_Neteasy_Mail():
         return ctx.call('MP.encrypt2', pwd)
 
 
-
-
-
 login = Login_Neteasy_Mail()
-print(login.login('983821194@163.com', 'whf'))
+print(login.login('wy983821194@163.com', 'whf454545'))
--- test.py
+++ test.py
@@ -5,20 +5,59 @@
 # @File    : test.py
 # @Software: PyCharm
 
-import execjs
+# import execjs
+#
+#
+# def get_js():
+#     f = open("encrypt.js", 'r', encoding='ISO-8859-1')
+#     line = f.readline()
+#     htmlstr = ''
+#     while line:
+#         htmlstr = htmlstr + line
+#         line = f.readline()
+#     return htmlstr
+#
+#
+# jsstr = get_js()
+# ctx = execjs.compile(jsstr)
+# pw = ctx.call('MP.encrypt2', 'whf454545')
+# print(pw)
 
+import requests
+import json
 
-def get_js():
-    f = open("encrypt.js", 'r', encoding='ISO-8859-1')
-    line = f.readline()
-    htmlstr = ''
-    while line:
-        htmlstr = htmlstr + line
-        line = f.readline()
-    return htmlstr
+session = requests.session()
 
+Headers = {
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Connection': 'keep-alive',
+    'Content-Length': '377',
+    'Content-Type': 'application/json',
+    'Cookie': 'starttime=; webzjcookiecheck=1; c98xpt_=30; _ihtxzdilxldP8_=30; l_s_mail163CvViHzl=2BDA1093FDDA9283AD02B57FFFEC7E0E248C665EC4BD4EE3D6EE49E2E12AFE3F13B9532FF4F2425CF0C0882767340A0DBCA8A9D75E74384B18886910685B9A89979A8FBA1FABB7B5AE5696049516591725D38AA7B59DDF9E33CEAAAF02AB4E49DC23E099BAB187057B7F68A5DDC0F53A; JSESSIONID-WYTXZDL=kcGOsa0MEP51UuSbXqSn%5CxvLjFKYekL2Md%5C%5CuaVMmER9H7UmOEmfPqJ1NqMMcWkKOd%2FaJKPnyy3a%5CwhjhmnFKH79Qa%2BA61Hy20wyjpNh%5C3SomVlZzXY7WesZuyD%2Bct8RZle1zav4VfLk%5CRlz3uag2u8KvKjfmaS3RsVI%2BYePWIWznxE9%3A1524631506997; jsessionid-cpta=%2BKPpx%2FNmuEKVqnNjvxKE%2BMg%2FF72EyDnAli6SfDvE%2BB7ZBy%2BWoVygj80tJ16vIwazwx%5C2BlmJcHitYWOk1Ec%2BoT9qM7CPtqtSleZ%2FwVnLipIfDRwwPOl%5CCGU%2BJ1OZbCb9LioqgOX3UE%2FOKHNV%2F%5CEQAGLc8FQHrM%2B6mOWDm8z6bQaKxBqE%3A1524631865046',
+    'Host': 'dl.reg.163.com',
+    'Origin': 'https://dl.reg.163.com',
+    'Referer': 'https://dl.reg.163.com/webzj/m163_2/pub/index_dl.html?wdaId=&pkid=CvViHzl&product=mail163',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 
-jsstr = get_js()
-ctx = execjs.compile(jsstr)
-pw = ctx.call('MP.encrypt2', 'whf454545')
-print(pw)
+}
+
+request_url = 'https://dl.reg.163.com/l'
+request_data = {
+    'd': 10,
+    'domains': '163.com',
+    'l': 0,
+    'pd': 'mail163',
+    'pwdKeyUp': '1',
+    'topURL': 'https://mail.163.com/',
+    'pkid': 'CvViHzl',
+    'tk': '4518fbe5ac6f8ea606c75ebd1dffdf6e',
+    'un': 'wy983821194@163.com',
+    't': 1524630976135,
+    'pw': 'V+pwW+EQNP1ARCXQqQROuBgzhjv98vsYWgKr7HhGftUyoP6rYFaOUJJppTGlbKwNP+XuBj8x3q127VMi5iY/WX0/T2e9pamg/7d4yig+q5BkDHDz/gmVeo4WFitfLvCyCKAh3HKcWDfOxyO+lVaVdM8bHt2kIQspUvAwI7zaBuA= '
+}
+
+resp = session.post(request_url, data=json.dumps(request_data), headers=Headers)
+print(resp.text)
+
